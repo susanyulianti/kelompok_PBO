8a1f628cf2d917ac22c0c8ae26fa6e5c63b8727c
regginakuswandi <regginakuswandi14@gmail.com>
2021-06-24 20:55:42 +0700

    coba

--- database/migrations/2021_06_24_124626_create_members_table.php
+++ database/migrations/2021_06_24_124626_create_members_table.php
@@ -15,6 +15,13 @@ class CreateMembersTable extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_lengkap', 100);
+            $table->string('kelas', 50);
+            $table->string('jurusan', 50);
+            $table->string('tanggal_lahir', 50);
+            $table->string('jenis_kelamin', 50);
+            $table->string('no_telp', 50);
+            $table->string('alamat', 50);
             $table->timestamps();
         });
     }
